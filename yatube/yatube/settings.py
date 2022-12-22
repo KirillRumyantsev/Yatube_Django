87a0c9f9918b01950c1bb03d6d802c15b0010655
Кирилл Румянцев <kirillsonytablet@gmail.com>
2022-12-22 22:31:32 +0300

    Visual

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -33,6 +33,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 MIDDLEWARE = [
@@ -43,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 MEDIA_URL = '/media/'
