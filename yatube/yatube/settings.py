d7c0f850cbc91f7b56714062f77624c3d9602674
Кирилл Румянцев <kirillsonytablet@gmail.com>
2022-12-23 14:38:36 +0300

    Public Pythonanywhere, debag image_create

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -13,6 +13,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.PetPythonProgect.pythonanywhere.com',
+    'PetPythonProgect.pythonanywhere.com',
 ]
 
 CACHES = {
@@ -48,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 MEDIA_URL = '/media/'
